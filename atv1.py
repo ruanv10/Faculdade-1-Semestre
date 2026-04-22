edbcf969e6dae5a16809470cfb49789b8be9dc3f
ruanv10 <ruanvitor.goncalves10@gmail.com>
2026-04-22 20:52:04 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,7 @@
+valor1 = int(input('Valor de A: '))
+valor2 = int(input('Valor de B'))
+quantidade = int(input('Quantidade de numero: '))
+
+média = valor1 + valor2 / quantidade
+
+print(f'Resultado: {média}')
